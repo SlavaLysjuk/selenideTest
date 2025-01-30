a69a68fccd231357c14952e3991a863078c33c1a
Slava Lysjuk <vlysjuk@gmail.com>
2025-01-30 22:58:35 +0200

    Add new HomeWork28 1

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -13,21 +13,27 @@ public class BaseTest {
     public static final Logger log = Logger.getLogger(BaseTest.class.getName());
 
     @BeforeMethod
-    @org.testng.annotations.Parameters({"browser"})
-    public void setUp(String browser) {
-        log.info("Test Method setUp");
-        Configuration.timeout = 1000;
+    public void setUp(Object[] params) {
+        if (params.length == 0 || !(params[0] instanceof String)) {
+            throw new IllegalArgumentException("Browser parameter is missing or invalid.");
+        }
+
+        String browser = (String) params[0];
+        log.info("Setting up browser: " + browser);
+
         Configuration.remote = "http://localhost:4444/wd/hub";
         Configuration.browser = browser;
-//        Configuration.browserVersion = "124.0";
+        Configuration.timeout = 10000;
+        Configuration.headless = false;
+        Configuration.baseUrl = ConfigReader.getBaseUrl();
+        Configuration.pollingInterval = 1000;
+
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.of(
                 "enableVNC", true,
                 "enableVideo", false
         ));
         Configuration.browserCapabilities = capabilities;
-        Configuration.baseUrl = ConfigReader.getBaseUrl();
-        Configuration.pollingInterval = 1000;
 
         open("/");
     }
--- src/test/java/QaAutoSelenoidTest.java
+++ src/test/java/QaAutoSelenoidTest.java
@@ -1,5 +1,6 @@
 import com.codeborne.selenide.Condition;
-import hillel.qa.BrowserFactory;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
@@ -8,9 +9,16 @@ import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$x;
 
-public class QaAutoSelenoidTest extends BaseTest{
+public class QaAutoSelenoidTest extends BaseTest {
 
-    @Test(dataProvider = "browsers", dataProviderClass = BrowserFactory.class)
+
+    @BeforeMethod
+    @Override
+    public void setUp(Object[] params) {
+        super.setUp(params);
+    }
+
+    @Test(dataProvider = "browsers")
     public void testCanStartOnSelenoid(String browser) {
         $x("//button[text()='Sign In']").shouldBe(visible).click();
         $("#signinEmail").shouldBe(visible).sendKeys("test@hillel.ua");
@@ -18,5 +26,13 @@ public class QaAutoSelenoidTest extends BaseTest{
         $x("//button[text()='Login']").shouldBe(visible).click();
         $x("//p[@class='alert alert-danger']").shouldBe(Condition.visible, Duration.ofSeconds(3))
                 .shouldHave(Condition.text("Wrong email or password"));
+    }
+
+    @DataProvider(name = "browsers", parallel = true)
+    public Object[][] getBrowsers() {
+        return new Object[][]{
+                {"chrome"},
+                {"firefox"}
+        };
     }
 }
