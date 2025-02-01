cf781d72c8cd7746425b1f9f6d2202815965aa6c
Slava Lysjuk <vlysjuk@gmail.com>
2025-02-01 11:09:19 +0200

    in BaseTest add remote configuration site

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,7 +1,9 @@
 import com.codeborne.selenide.Configuration;
 import hillel.qa.ConfigReader;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.BeforeMethod;
 
+import java.util.Map;
 import java.util.logging.Logger;
 
 import static com.codeborne.selenide.Selenide.open;
@@ -12,12 +14,29 @@ public class BaseTest {
 
     @BeforeMethod
     public void setUp() {
+//        log.info("Test Method setUp");
+//        Configuration.timeout = 1000;
+//        Configuration.browser = "chrome";
+//        Configuration.headless = true;
+//        Configuration.baseUrl = ConfigReader.getBaseUrl();
+//        Configuration.pollingInterval = 1000;
+//
+//        open("/");
+
         log.info("Test Method setUp");
-        Configuration.timeout = 1000;
+        Configuration.remote = "http://localhost:4444/wd/hub";
         Configuration.browser = "chrome";
-        Configuration.headless = true;
+        Configuration.timeout = 10000;
+        Configuration.headless = false;
         Configuration.baseUrl = ConfigReader.getBaseUrl();
         Configuration.pollingInterval = 1000;
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.of(
+                "enableVNC", true,
+                "enableVideo", false
+        ));
+        Configuration.browserCapabilities = capabilities;
 
         open("/");
     }
