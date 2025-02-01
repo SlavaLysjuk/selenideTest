b9a15395681fe84f255f2bf10846ab78fb141d57
Slava Lysjuk <vlysjuk@gmail.com>
2025-02-01 09:28:13 +0200

    Add new HomeWork29 1

--- src/test/java/AddCarJenkinsAllureTest.java
+++ src/test/java/AddCarJenkinsAllureTest.java
@@ -1,4 +1,8 @@
+import org.testng.annotations.Test;
+
 public class AddCarJenkinsAllureTest extends BaseTest{
+
+    @Test
     public static void main(String[] args) {
         System.out.println("Test 1");
     }
