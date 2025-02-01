cf3c855cb6cc1e79769d209de3762bf675ec360e
Slava Lysjuk <vlysjuk@gmail.com>
2025-02-01 12:58:19 +0200

    change browser=chrome

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -25,9 +25,9 @@ public class BaseTest {
 
         log.info("Test Method setUp");
 //        Configuration.remote = "http://localhost:4444/wd/hub";
-        Configuration.browser = "firefox";
+        Configuration.browser = "chrome";
         Configuration.timeout = 10000;
-        Configuration.headless = true;
+//        Configuration.headless = true;
         Configuration.baseUrl = ConfigReader.getBaseUrl();
         Configuration.pollingInterval = 1000;
 
